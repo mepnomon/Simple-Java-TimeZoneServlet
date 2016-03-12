7a8c64de45df27b44d7079e503ee192301e05f6d
James <eeu48c@bangor.ac.uk>
2016-03-12 18:07:41 +0000

    Task 4: Added back-end logic

--- src/main/java/Task4/LoginServlet.java
+++ src/main/java/Task4/LoginServlet.java
@@ -30,6 +30,11 @@ public class LoginServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        
+        // Gets the parameters out of the request
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+                
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
@@ -38,7 +43,8 @@ public class LoginServlet extends HttpServlet {
             out.println("<title>Servlet LoginServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+            out.println("<p>Hi user " + username + ". You are now logged into the system</p>");
+            out.println("<h1>PASSWWORD: " + password + "</h1>);
             out.println("</body>");
             out.println("</html>");
         }
